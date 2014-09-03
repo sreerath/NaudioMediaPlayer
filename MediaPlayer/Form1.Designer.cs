46f4221fa60a717231eab32a1455c172481f1628
sreerath <sreeradhakrishnan@gmail.com>
2014-09-02 23:11:37 -0700

    Naudio
    Media Player

---
+++ MediaPlayer/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace MediaPlayer
+{
+    partial class Forward
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Browse = new System.Windows.Forms.Button();
+            this.Play = new System.Windows.Forms.Button();
+            this.Pause = new System.Windows.Forms.Button();
+            this.FileName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Rewind = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(461, 40);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 23);
+            this.Browse.TabIndex = 0;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(45, 227);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(75, 23);
+            this.Play.TabIndex = 1;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // Pause
+            // 
+            this.Pause.Location = new System.Drawing.Point(288, 227);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(75, 23);
+            this.Pause.TabIndex = 2;
+            this.Pause.Text = "Pause";
+            this.Pause.UseVisualStyleBackColor = true;
+            this.Pause.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // FileName
+            // 
+            this.FileName.Location = new System.Drawing.Point(45, 42);
+            this.FileName.Name = "FileName";
+            this.FileName.Size = new System.Drawing.Size(391, 20);
+            this.FileName.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(126, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = ">>30sec";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Rewind
+            // 
+            this.Rewind.Location = new System.Drawing.Point(207, 227);
+            this.Rewind.Name = "Rewind";
+            this.Rewind.Size = new System.Drawing.Size(75, 23);
+            this.Rewind.TabIndex = 5;
+            this.Rewind.Text = "<<30sec";
+            this.Rewind.UseVisualStyleBackColor = true;
+            this.Rewind.Click += new System.EventHandler(this.Rewind_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Total song lenth";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(315, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 8;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(45, 176);
+            this.trackBar1.Maximum = 30000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(374, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 250;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(45, 68);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(374, 69);
+            this.listBox1.TabIndex = 9;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(369, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Forward
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Rewind);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.FileName);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.Browse);
+            this.Name = "Forward";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.TextBox FileName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Rewind;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MediaPlayer/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave.SampleProviders;
+using NAudio.Wave;
+using System.IO;
+using NAudio;
+using NAudio.CoreAudioApi;
+
+
+
+
+
+namespace MediaPlayer
+{
+    public partial class Forward : Form
+    {
+        TrackBar tbrSlider;
+
+        public Forward()
+        {
+            InitializeComponent();
+        }
+   
+
+        public string str;
+        OpenFileDialog open = new OpenFileDialog();
+        private NAudio.Wave.WaveFileReader wave = null;
+       
+       public  WaveIn wi;
+       public WaveOut wo;
+       public AudioFileReader aud;
+    
+       public static int myrec = 0;
+       public static int count = 0;
+       long[] myArray1=new long[20];
+       string[] myArray2=new string[8];
+
+
+        public void Browse_Click(object sender, EventArgs e)
+        {
+            
+            open.Filter = "Wave or mp3 files(*.wav or *.mp3)|*.mp3";
+            
+            if (open.ShowDialog() == DialogResult.OK)          
+               FileName.Text = open.FileName;
+               
+        }
+
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            
+            Disposewave();
+            wo = new WaveOut();
+            aud = new AudioFileReader(open.FileName);
+            wo.Init(aud);
+            wo.Play();
+
+            string duration = aud.TotalTime.ToString("mm\\:ss");
+            trackBarcontrol(0);                  
+            label1.Text = duration;
+
+        }
+
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            if(wo !=null)
+                if (wo.PlaybackState == NAudio.Wave.PlaybackState.Playing)
+                {
+                    
+                    wo.Pause();
+                    myrec++;
+
+                    long newPos = aud.Position;
+                    
+                    string curmin = aud.CurrentTime.Minutes.ToString();
+                    string cursec = aud.CurrentTime.Seconds.ToString();
+
+                    label3.Text = curmin + ":" + cursec;
+
+                    listBox1.Items.Add(label3.Text);
+                    myArray2[myrec] = curmin + ":" + cursec;
+                    myArray1[myrec] = newPos;
+                                   
+                    wo.Play();
+                }
+
+          }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int i = 0;
+
+            foreach (string str in myArray2)
+            {
+                i++;
+
+                if ((string)listBox1.SelectedItem == str)
+                {
+                    aud.Position = myArray1[i];
+                                    
+                    
+                    wo.Init(aud);
+                    wo.Play();
+                    trackBarcontrol(0);
+                    break;
+                }
+            }
+        }
+        
+
+
+        private void Disposewave()
+        {
+            if(wo!=null)
+            {
+                if(wo.PlaybackState==NAudio.Wave.PlaybackState.Playing) wo.Stop();
+                wo.Dispose();
+                     
+            }
+
+       }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (wo.PlaybackState == NAudio.Wave.PlaybackState.Playing) wo.Stop();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Disposewave();
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+
+         /* long newPos = aud.Position + (long)(aud.WaveFormat.AverageBytesPerSecond *30);
+
+            if (newPos > aud.Length)
+                aud.Position = aud.Length;
+            else if (newPos < 0)
+                aud.Position = 0;
+            else
+               aud.Position = newPos;*/
+                   
+                      
+            aud.Skip(+30);
+            wo.Init(aud);
+            
+            wo.Play();
+            trackBarcontrol(+30);
+            
+        }
+
+        private void Rewind_Click(object sender, EventArgs e)
+        {
+            //aud.CurrentTime=aud.CurrentTime.Subtract(new TimeSpan(0,0,0,30));
+            aud.Skip(-30);
+            wo.Init(aud);
+            wo.Play();
+            
+            trackBarcontrol(-30);
+        }
+   
+      
+
+        /*private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+            trackBar1.Value = 60 * (int)aud.TotalTime.TotalMinutes;
+
+        }*/
+
+        private void trackBarcontrol(int val)
+        {
+            trackBar1.Minimum = 0;
+            trackBar1.Maximum = (int)aud.TotalTime.TotalSeconds;
+            timer2.Start();
+            
+            if(val==0)
+            {
+                timer2.Enabled = true;
+               
+                trackBar1.Value = 0;
+            }
+                           
+            trackBar1.Value = 0;
+            if (val == +30)
+                trackBar1.Value = trackBar1.Value +30;
+
+            if (val == -30 )
+            {
+                if (trackBar1.Value<=30)
+                     trackBar1.Value=30;
+                else
+                    trackBar1.Value = trackBar1.Value - 30;
+            }
+          
+            
+            
+            
+          
+       }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            string curmin = aud.CurrentTime.Minutes.ToString();
+            string cursec = aud.CurrentTime.Seconds.ToString();
+            label3.Text = aud.CurrentTime.ToString("mm\\:ss");
+            
+            if(count==1)
+                trackBar1.Value = (int)aud.CurrentTime.Seconds+59; 
+            else 
+                trackBar1.Value =  (int)aud.CurrentTime.Seconds;
+
+            if ((int)aud.CurrentTime.Seconds == 59)
+                
+                count = count + 1;
+            
+               // trackBar1.Value = trackBar1.Value + 1;
+                 
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+            if (wo.PlaybackState == NAudio.Wave.PlaybackState.Playing)
+                trackBar1.Value = trackBar1.Value + 1;
+
+        }
+
+      
+        
+       
+
+  
+       
+    }
+   
+}
